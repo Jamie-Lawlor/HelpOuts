120f654a543d3e34d2489ea26d43f5620811d6a2
Luke Hilliard <119594459+LukeHilliard@users.noreply.github.com>
2025-11-18 14:51:28 +0000

    small seeding
    setting up seeding for flask was a bit more complex than i expected. For the sake of moving on development i've written some sql that everyone can use after running the migrations. it will create two users based on the personas and a job they completed.

---
+++ db/seed.sql
@@ -0,0 +1,20 @@
+
+INSERT INTO users (name, email, password, type, work_area, specialism, skills, rating)
+VALUES
+('Bridget Muckian', 'bridgetm1@gmail.com', 'Test1234567!', 'helpee', 'Crossmaglen', '', '', '', None),
+("Ryan O'Hare", 'ryanohare@gmail.com', 'Test1234567!', 'helper', 'Crossmaglen', 'Contractor', 'Home Maintinace, General Maintenance, Carpentry', 3),
+
+
+INSERT INTO user_permissions (user_id, accepted_terms, accepted_gdpr, accepted_health_safety)
+VALUES
+(1, FALSE, FALSE, FALSE),
+(2, FALSE, FALSE, FALSE);
+
+INSERT INTO reviews (helper_id, helpee_id, job_id, star_rating, review)
+VALUES
+(2, 1, 1, 5, 'Ryan was fantastic! He helped me with my home repairs and was very professional.');
+
+INSERT INTO jobs ( helper_id, helpee_id, status, area, job_title, job_description, short_title, short_type, created_date, start_date, end_date)
+VALUES
+(2, 1, 'C', 'Crossmaglen', 'Home Repairs', 'If anyone out there would be nice enough to come to my house and change a few of my outside lightbulbs. I can provide the LED spotlight bulbs required but just need a capable person to put them in!',
+ 'Home Repairs' 'Home', CURRENT_DATE, '2026-01-21', '2026-01-21');
