b069ac7c4c3f42ad6bceccf9c14525046e8c18b1
Luke Hilliard <119594459+LukeHilliard@users.noreply.github.com>
2025-11-23 16:02:10 +0000

    Register adds user to db
    I've updated the register_helpee/ route to now add the user to the database. I've added a check to see if a user with the entered email exists, if true an error message is passed back to the register page. I've left space for security & validation to be added

--- routes/login.py
+++ routes/login.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect
-
+from db.database import db
+from db.modals import Users
 login_blueprint = Blueprint("login", __name__, template_folder="templates")
 
 
@@ -20,4 +21,27 @@ def register_helpee():
     email = request.form.get("email")
     location = request.form.get("location")
     password = request.form.get("password")
+    
+    
+    # check if this users already exists
+    if_exists = Users.query.filter_by(email=email).first()
+    if if_exists:
+        # this error message is passed to the frontend and can be used to display an error to the user
+        error = "A user with this email already exists"
+        return render_template("/register_helpee/", error=error) 
+    
+    # validate password & user details
+    
+    
+    user = Users(
+        name=first_name + " " + last_name,
+        email=email,
+        password=password,
+        type="helpee",
+        work_area=location,
+        rating=0
+        )
+    db.session.add(user)
+    db.session.commit()
+    
     return redirect("/home_page/")
