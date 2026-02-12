105a820d2e408a001b2a5444d5ec3fc643a612da
D00253579 <D00253579@student.dkit.ie>
2026-02-12 16:59:23 +0000

    Fixing minor issues
    Fixing some issues that we fixed for T-CA2 on AWS but did not push to github

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, session
 import os
 from db.database import db
-from db.models import Users, Messages, Communities
 from flask_migrate import Migrate
 from routes.login import login_blueprint
 from routes.messages import messages_blueprint
@@ -9,18 +8,16 @@ from routes.profile import profile_blueprint
 from routes.posts import posts_blueprint
 from routes.subscriptions import subscriptions_blueprint
 from dotenv import load_dotenv
-from flask_socketio import SocketIO, join_room, send
-from datetime import datetime
-from Crypto.Cipher import PKCS1_OAEP
-from Crypto.PublicKey import RSA
+from events import socketio
 load_dotenv()
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URL")
 app.secret_key = os.getenv("SECRET_KEY")
-socketio = SocketIO(app)
 db.init_app(app)
+socketio.init_app(app)
+
 migrate = Migrate(app, db)
 app.register_blueprint(login_blueprint)
 app.register_blueprint(messages_blueprint)
@@ -32,13 +29,11 @@ app.register_blueprint(subscriptions_blueprint)
 @app.route("/")
 def index():
     session["id"] = 3
-    print("USER ID: ",session.get("user_id"))
     return render_template("index.html")
 
 
 @app.route("/home_page/")
 def home_page():
-    print("USER ID: ",session.get("user_id"))
     return render_template("home_page.html")
 
 
@@ -50,52 +45,6 @@ def settings_page():
 @app.route("/helper_settings/")
 def helper_settings_page():
     return render_template("helper_settings.html")
-
-
-@socketio.on("join_room")
-def handle_connect():
-    session["room"] = "user_3_user_5_direct_messaging"
-    room = session.get("room")
-    join_room(room)
-
-
-
-@socketio.on("message_sent")
-def message_sent(data):
-    date = datetime.now()
-    room = session.get("room")
-    sender_id = data["sender_id"]
-    receiver_id = data["receiver_id"]
-    sender = Users.query.get_or_404(2)
-    # receiver = Users.query.get_or_404(2)
-    if sender_id == "1":
-        sender_data = Communities.query.get_or_404(sender_id)
-    else:
-        sender_data = Users.query.get_or_404(sender_id)
-    
-    message = data["message"]
-
-    public_key = sender.public_key
-
-    RSA_public_key = RSA.import_key(public_key)
-    cipher = PKCS1_OAEP.new(RSA_public_key)
-    encrypted_message = cipher.encrypt(message.encode('utf-8'))
-
-    messageContent = {
-        "user": sender_data.name,
-        "message": message,
-        "sent": date.strftime("%H:%M"),
-        "profile_picture":sender_data.profile_picture
-    }
-    message = Messages(
-        sender_id=sender_id,
-        receiver_id=receiver_id,
-        content=encrypted_message,
-        timestamp = date
-    )
-    db.session.add(message)
-    db.session.commit()
-    send(messageContent, to = room)
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
---
+++ events.py
@@ -0,0 +1,55 @@
+from extensions import socketio
+from flask import session
+from flask_socketio import join_room, send
+from db.database import db
+from db.models import Users, Messages, Communities
+from datetime import datetime
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+
+
+@socketio.on("join_room")
+def handle_connect():
+    session["room"] = "user_3_user_5_direct_messaging"
+    room = session.get("room")
+    join_room(room)
+
+
+
+@socketio.on("message_sent")
+def message_sent(data):
+    date = datetime.now()
+    room = session.get("room")
+    sender_id = data["sender_id"]
+    receiver_id = data["receiver_id"]
+    sender = Users.query.get_or_404(2)
+    # receiver = Users.query.get_or_404(2)
+    if sender_id == "1":
+        sender_data = Communities.query.get_or_404(sender_id)
+    else:
+        sender_data = Users.query.get_or_404(sender_id)
+    
+    message = data["message"]
+
+    public_key = sender.public_key
+
+    RSA_public_key = RSA.import_key(public_key)
+    cipher = PKCS1_OAEP.new(RSA_public_key)
+    encrypted_message = cipher.encrypt(message.encode('utf-8'))
+
+    messageContent = {
+        "user": sender_data.name,
+        "message": message,
+        "sent": date.strftime("%H:%M"),
+        "profile_picture":sender_data.profile_picture
+    }
+    message = Messages(
+        sender_id=sender_id,
+        receiver_id=receiver_id,
+        content=encrypted_message,
+        timestamp = date
+    )
+    db.session.add(message)
+    db.session.commit()
+    send(messageContent, to = room)
+
