14b541f9b0fc5e7ad7b792f869f4c3b53a971cea
unknown <katielynch2204@gmail.com>
2026-03-04 14:29:34 +0000

    Alternative MFA
    Using MFA by sending the user an email instead of using their phone number

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, session, send_file, request, redirect
+from flask_mail import Mail, Message
 import os
 from db.database import db
 from db.models import Users
@@ -12,14 +13,23 @@ from routes.api import api_blueprint
 from werkzeug.security import check_password_hash
 from dotenv import load_dotenv
 from events import socketio
+import pyotp
 load_dotenv()
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URL")
+app.config["MAIL_SERVER"] = os.getenv("GMAIL_SERVER")
+app.config["MAIL_PORT"] = int(os.getenv("GMAIL_PORT", 587))
+app.config["MAIL_USE_TLS"] = os.getenv("GMAIL_TLS") == "True"
+sender = app.config["MAIL_USERNAME"] = os.getenv("GMAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.getenv("GMAIL_PASSWORD")
+
+
 app.secret_key = os.getenv("SECRET_KEY")
 db.init_app(app)
 socketio.init_app(app)
+mail = Mail(app)
 
 migrate = Migrate(app, db)
 app.register_blueprint(login_blueprint)
@@ -71,13 +81,49 @@ def login():
         password_check = check_password_hash(hashed_password, password)    
 
     if user is not None and password_check:
-        session["user_id"] = user.id
-        session["profile_picture"] = user.profile_picture
-        session["type"] = user.type
-        return redirect("/home_page/")
+        session["email"] = email
+        session.pop("otp", None)
+        return redirect("/mfa")
     else:
         error = "Email or Password Is Incorrect"
         return render_template("login/login.html", error = error)
+
+@app.route("/mfa", methods=["GET", "POST"])
+def mfa():
+    if "email" not in session:
+        return redirect("/login")
+    
+    user = Users.query.filter_by(email=session["email"]).first()
+    
+    if request.method == "POST":
+        otp = request.form["otp"]
+        mfa_session = session.get("otp")
+        
+        if mfa_session:
+            totp = pyotp.TOTP(mfa_session)
+            if totp.verify(otp):
+                session.pop("email", None)
+                session["user_id"] = user.id
+                session["profile_picture"] = user.profile_picture
+                session["type"] = user.type
+                return redirect("/home_page/")
+        else:
+            error = "Incorrect One Time Password, Please Try Again."
+            return render_template("login/mfa.html", error = error)
+    
+
+    if "otp" not in session:
+        pyotp_secret = pyotp.random_base32()
+        session["otp"] = pyotp_secret
+
+        totp = pyotp.TOTP(pyotp_secret)
+        otp_code = totp.now()
+
+        message = Message("HelpOuts Verification", sender=sender, recipients=[user.email])
+        message.body = f"Your One Time Passcode is: {otp_code}"
+        mail.send(message)
+    
+    return render_template("login/mfa.html")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
