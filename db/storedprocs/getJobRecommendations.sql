17d078f963315415bf533eb857b74777f597c1f6
Luke Hilliard <119594459+LukeHilliard@users.noreply.github.com>
2026-03-08 21:03:02 +0000

    returned all job data in stored proc
    i have changed the stored proc to return all of the job data instead of only some

--- db/storedprocs/getJobRecommendations.sql
+++ db/storedprocs/getJobRecommendations.sql
@@ -4,27 +4,29 @@ DELIMITER //
     )
        BEGIN
         SELECT
-            j.id,
-            j.job_title,
-            j.status,
-            j.area,
+            j.*,
             u.id AS user_id,
             u.name,
             COUNT(DISTINCT js.skill_id) AS match_score
-        FROM
-            jobs AS j
+        FROM jobs AS j
             INNER JOIN job_skills AS js ON j.id = js.job_id
             INNER JOIN user_skills AS us ON js.skill_id = us.skill_id
             INNER JOIN users AS u ON us.user_id = u.id
         WHERE
-            u.id = 3
+            u.id = userId
         GROUP BY
             j.id,
             j.job_title,
+            j.job_description,
+            j.short_title,
+            j.short_type,
             j.status,
             j.area,
-            u.id,
-            u.name
+            j.created_date,
+            j.start_date,
+            j.end_date,
+            j.project_id,
+            u.id
         ORDER BY
             match_score DESC,
             j.id;
